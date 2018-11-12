bee04fe75a7f4cec9226a84d674ffa676f597fdd
Peter Krivan <krivan.peter@outlook.com>
2018-11-12 16:35:24 +0000

    rotates deleted, position added by arguments

--- assets/js/note.js
+++ assets/js/note.js
@@ -1,9 +1,12 @@
 class Note {
-    constructor(color, x, y) {
+    constructor(color, x, y, angle1, angle2) {
         this.color = color;
         this.x = x;
         this.y = y;
         this.r;
+        this.ang1 = angle1;
+        this.ang2 = angle2;
+        this.glow_c = this.color;
     }
     show() {
         if (sketchCanvas.width > sketchCanvas.height) {
@@ -13,7 +16,20 @@ class Note {
             this.r = sketchCanvas.width * 0.8;
         }
         fill(this.color);
-        arc(this.x, this.y, this.r, this.r, 0, HALF_PI, PIE);
-        rotate(PI / 2);
+        arc(this.x, this.y, this.r, this.r, this.ang1, this.ang2, PIE);
+    }
+
+    glow() {
+        var d = dist(this.x, this.y, mouseX, mouseY);
+        if (d < this.r) {
+            if (this.glow_c == this.color) {
+                this.color = [255, 255, 255];
+            }
+            else if (this.glow_c !== this.color) {
+                this.color = this.glow_c;
+            }
+            fill(this.color);
+            arc(this.x, this.y, this.r, this.r, this.ang1, this.ang2, PIE);
+        }
     }
 }
--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,13 +1,15 @@
 var canvas;
 var width;
 var sketchCanvas;
+var pi = 3.14;
 
-var note_green = new Note([0, 255, 0], 0, 0);
-var note_red = new Note([255, 0, 0], 0, 0);
-var note_blue = new Note([0, 0, 255], 0, 0);
-var note_yellow = new Note([255, 255, 0], 0, 0);
+var note_green = new Note([0, 255, 0], 0, 0, pi, 1.5*pi);
+var note_red = new Note([255, 0, 0], 0, 0, 1.5 * pi, 2 * pi);
+var note_blue = new Note([0, 0, 255], 0, 0, 2 * pi, 2.5 * pi);
+var note_yellow = new Note([255, 255, 0], 0, 0, 2.5 * pi, pi);
 
 function setup() {
+    translate(width / 2, height / 2);
     /*Canvas creating*/
     canvas = document.getElementById('game-screen');
     width = canvas.offsetWidth;
@@ -16,19 +18,23 @@ function setup() {
     background(255, 255, 255, 100);
     /*SIMON NOTES*/
     translate(width / 2, height / 2);
-    rotate(PI);
     strokeWeight(10);
 
     note_green.show();
     note_red.show();
     note_blue.show();
     note_yellow.show();
-    innerCircle();
+
 }
 
 function draw() {
+    translate(width / 2, height / 2);
+    innerCircle();
     
+}
 
+function mouseClicked() {
+    note_green.glow();
 }
 
 function innerCircle() {
@@ -42,7 +48,6 @@ function innerCircle() {
         ellipse(0, 0, sketchCanvas.width * 0.45);
     }
     /*The ,,SIMON" text*/
-    rotate(PI);
     fill(255, 255, 255);
     if (sketchCanvas.width > sketchCanvas.height) {
         textSize(40);
